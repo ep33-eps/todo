5c7852c8020d1a8487c28f3dfc043c7018316196
ep33-eps <eps@techv.in>
2018-10-21 08:03:02 +0000

    todo app push

---
+++ src/main/java/docker/TodoDocker2Application.java
@@ -0,0 +1,12 @@
+package docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDocker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDocker2Application.class, args);
+	}
+}
